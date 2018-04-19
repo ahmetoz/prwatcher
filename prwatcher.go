fb9bf044646e00ed5c37f6afe7005722b3474b59
ahmetoz <bilmuhahmet@gmail.com>
2018-04-19 19:59:23 +0300

    add environment variable support for flags && some error handling enhancements

--- prwatcher.go
+++ prwatcher.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"fmt"
 )
 
 type PullRequestsResponse struct {
@@ -47,7 +48,15 @@ func getPullRequests(host, project, repository, token string) *PullRequestsRespo
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 
-	log.Debug(string(body))
+	if res.StatusCode != 200 {
+		log.Error("Getting pull requests, failed. Status: ", res.StatusCode)
+	}
+
+	if res.StatusCode == 403 || res.StatusCode == 401 {
+		panic(string(body))
+	}
+
+	fmt.Println(string(body))
 
 	if err != nil {
 		log.Error(err)
@@ -104,7 +113,8 @@ func getTriggerList(pull_requests *PullRequestsResponse) []int {
 		updateDate, exists := active_pr_list[pr.ID]
 		if exists {
 			if updateDate == 0 {
-				log.Info("PR: ", pr.ID, " will be skipped because closed")
+				log.Info("PR: ", pr.ID, " will be tried again.")
+				active_pr_list[pr.ID] = pr.UpdatedDate
 			}
 			if pr.UpdatedDate == updateDate {
 				log.Info("PR: ", pr.ID, " open but this version triggered before. Will be skipped.")
@@ -128,12 +138,21 @@ func triggerToUri(id int, trigger_uri string) {
 	log.Info("sending post request to ", url)
 
 	req, _ := http.NewRequest("POST", url, nil)
+
 	res, _ := http.DefaultClient.Do(req)
+	if res.StatusCode != 201 {
+		log.Error("PR: " , id, " trigger failed. Status: ", res.StatusCode)
+		active_pr_list[id] = 0 //0 means try later
+	}
 
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-	log.Debug(string(body))
+	if res.StatusCode == 403 || res.StatusCode == 401 {
+		panic(string(body))
+	}
+
+	fmt.Println(string(body))
 }
 
 func main() {
@@ -157,41 +176,43 @@ func main() {
 			Name:  "host",
 			Value: "",
 			Usage: "host address of docker registry",
+			EnvVar: "HOST",
 		},
 		cli.StringFlag{
 			Name:  "project",
 			Value: "",
 			Usage: "Only projects for which the authenticated user has the PROJECT_VIEW permission will be returned.",
+			EnvVar: "PROJECT",
 		},
 		cli.StringFlag{
 			Name:  "repository",
 			Value: "latest",
 			Usage: "The authenticated user must have REPO_READ permission for the specified project to call this resource.",
+			EnvVar: "REPOSITORY",
 		},
 		cli.StringFlag{
 			Name:  "username",
 			Value: "",
 			Usage: "stash user name",
+			EnvVar: "USERNAME",
 		},
 		cli.StringFlag{
 			Name:  "password",
 			Value: "",
 			Usage: "stash user password",
+			EnvVar: "PASSWORD",
 		},
 		cli.StringFlag{
 			Name:  "trigger_uri",
 			Value: "",
 			Usage: "job trigger uri - pr id will be added as query string to uri",
-		},
-		cli.StringFlag{
-			Name:  "pre_trigger",
-			Value: "",
-			Usage: "check uri before triggering job",
+			EnvVar: "TRIGGER_URI",
 		},
 		cli.StringFlag{
 			Name:  "duration",
 			Value: "@every 5m",
 			Usage: "job duration https://godoc.org/github.com/robfig/cron#hdr-Intervals",
+			EnvVar: "DURATION",
 		},
 	}
 
